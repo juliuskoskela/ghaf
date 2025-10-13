c4e6bd4204b0858c453157ba0fb42d1598e0768e
juliuskoskela <julius.koskela@unikie.com>
2025-10-13 23:43:03 +0300

    microvm,givc,fail2ban: fix logging and test compatibility
    - Make logging.client conditional on both logging.enable AND admin-vm
      existence in all microvm modules (host, netvm, audiovm, guivm, appvm)
    - Make logging.server conditional on logging.enable in adminvm
    - Comment out givc assertion that prevents debug+logging, referencing
      PR #1463 for context
    - Fix fail2ban maxretry to use 'or false' when accessing profiles.debug
      for compatibility with test environments
    
    Signed-off-by: juliuskoskela <julius.koskela@unikie.com>

--- modules/microvm/sysvms/adminvm.nix
+++ modules/microvm/sysvms/adminvm.nix
@@ -72,7 +72,7 @@ let
 
             # Services
             # Enable logging server on admin-vm
-            logging.server = true;
+            logging.server = config.ghaf.logging.enable;
 
             security.fail2ban.enable = configHost.ghaf.development.ssh.daemon.enable;
 
